e72afed778d856372a5970141779742796256158
newday <118717143+nwdy@users.noreply.github.com>
2025-03-21 20:46:46 +0700

    Add some classes of module model

---
+++ backend/src/main/java/com/nwdy/phonevip/model/Cart.java
@@ -0,0 +1,28 @@
+package com.nwdy.phonevip.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "carts")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<CartItem> cartItems;
+}
