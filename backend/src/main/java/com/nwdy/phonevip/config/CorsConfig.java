a2b7a8406335e3c69400abd8fbe6f61221b540ae
newday <118717143+nwdy@users.noreply.github.com>
2025-04-16 17:43:56 +0700

    Update frontend configuration for Docker Compose integration

--- backend/src/main/java/com/nwdy/phonevip/config/CorsConfig.java
+++ backend/src/main/java/com/nwdy/phonevip/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:5500", privateOrigin)
+                .allowedOrigins("http://localhost", "http://127.0.0.1:5500", privateOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
